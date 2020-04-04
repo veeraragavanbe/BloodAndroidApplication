ed868d854689676b700f4727d223e10742e772e9
veera <veeraragavanbe@gmail.com>
2020-04-04 11:53:35 +0530

    Single commit full project

--- php/dbinsert.php
+++ php/dbinsert.php
@@ -12,16 +12,7 @@ $phone=$_GET['phone'];
 $email=$_GET['email'];
 $address1=$_GET['address'];
 $city1=$_GET['city'];
-
-
 $connection=mysqli_connect($host,$user,$pass,$db_name);
-
 $sql="INSERT INTO `register`(`Name`, `Age`, `Blood_Group`, `Mobile`, `Email`, `Address`, `City`) VALUES ('$name','$age','$blood','$phone','$email','$address1','$city1')";
-
-
 $result=mysqli_query($connection,$sql);
-
-
-
-
 ?>
